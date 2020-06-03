e23cd2b2986b191e54babc267e85e72ce16f2317
Stephen Bell <quabbin@pop-os.localdomain>
2020-06-03 13:15:33 -0400

    Create the Phases component

---
+++ src/components/Phases.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Phase from "./Phase.jsx";
+
+class Phases extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      phase: {},
+    };
+  }
+
+  setPhaseInfo(file) {}
+
+  render() {
+    <div>
+      <h1>We are currently in Phase 1.</h1>
+      <h3>Phase 2 is projected to start on June 8th.</h3>
+      <div>
+        <button>Phase 1</button>
+        <button>Phase 2</button>
+        <button>Phase 3</button>
+        <button>Phase 4</button>
+      </div>
+    </div>;
+  }
+}

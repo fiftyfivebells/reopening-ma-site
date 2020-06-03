a0188b4cfc1652821820e195eac97ec59840318f
Stephen Bell <quabbin@pop-os.localdomain>
2020-06-03 16:34:30 -0400

    Set initial state to businesses

--- src/components/Phase.jsx
+++ src/components/Phase.jsx
@@ -4,6 +4,10 @@ class Phase extends React.Component {
   constructor(props) {
     super(props);
     this.updateInfo = this.updateInfo.bind(this);
+    this.state = {
+      active: "businesses",
+    };
+  }
 
   updateInfo(topic) {
     this.setState({ active: topic });

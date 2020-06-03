b3e8ea0ebb1e3a77768191ea1e5b8fac2c23a1f5
Stephen Bell <quabbin@pop-os.localdomain>
2020-06-03 13:25:40 -0400

    Fix imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Phases from "./components/Phases";
+import "./assets/styles/App.css";
 
 function App() {
   return (

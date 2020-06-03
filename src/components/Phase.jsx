d6bd3efc1dd15054496d71b51a7d560f85ae23b1
Stephen Bell <quabbin@pop-os.localdomain>
2020-06-03 16:34:00 -0400

    Make the updateInfo function and bind it to the class

--- src/components/Phase.jsx
+++ src/components/Phase.jsx
@@ -3,6 +3,10 @@ import React from "react";
 class Phase extends React.Component {
   constructor(props) {
     super(props);
+    this.updateInfo = this.updateInfo.bind(this);
+
+  updateInfo(topic) {
+    this.setState({ active: topic });
   }
 
   render() {

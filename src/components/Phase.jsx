4d6266e01c5beac6c2101aabf864befae23b0f3d
Stephen Bell <quabbin@pop-os.localdomain>
2020-06-03 16:34:49 -0400

    Render the phase info using buttons with click handlers

--- src/components/Phase.jsx
+++ src/components/Phase.jsx
@@ -14,6 +14,47 @@ class Phase extends React.Component {
   }
 
   render() {
-    return <div></div>;
+    const phase = this.props.phase;
+
+    if (Object.keys(phase).length === 0) {
+      return <div></div>;
+    } else {
+      const activeInfo = phase[this.state.active];
+      return (
+        <div>
+          <h2>
+            Starts: {phase.start_date}, Ends: {phase.end_date}
+          </h2>
+          <div>
+            <button
+              id="businesses"
+              onClick={(e) => this.updateInfo(e.target.id)}
+            >
+              Business
+            </button>
+            <button
+              id="hospitals"
+              onClick={(e) => this.updateInfo(e.target.id)}
+            >
+              Healthcare
+            </button>
+            <button id="advisory" onClick={(e) => this.updateInfo(e.target.id)}>
+              Advisory
+            </button>
+            <button id="outdoor" onClick={(e) => this.updateInfo(e.target.id)}>
+              Outdoor
+            </button>
+          </div>
+          <h3>{this.state.active}</h3>
+          <ul>
+            {activeInfo.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
   }
 }
+
+export default Phase;

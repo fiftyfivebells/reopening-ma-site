706c4c1ef16c44ca935cd746ef6e5de49fed86fc
fiftyfivebells <stephen.l.bell@gmail.com>
2020-06-11 20:01:46 -0400

    Create skeleton component for CitizenInfo

---
+++ src/components/CitizenInfo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../assets/styles/components.css";
+
+class CitizenInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div>HI</div>;
+  }
+}
+
+export default CitizenInfo;

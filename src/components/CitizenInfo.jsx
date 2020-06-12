293dfd6a50b2567179681ee20306db7c59af366c
fiftyfivebells <stephen.l.bell@gmail.com>
2020-06-11 23:28:58 -0400

    Implement the CitizenInfo functionality

--- src/components/CitizenInfo.jsx
+++ src/components/CitizenInfo.jsx
@@ -1,16 +1,47 @@
 import React from "react";
-
+import ListItem from "./ListItem";
 import "../assets/styles/components.css";
 
 class CitizenInfo extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.updateInfo = this.updateInfo.bind(this);
+    this.state = {
+      active: "",
+    };
+  }
+
+  updateInfo(topic) {
+    this.setState({ active: topic });
   }
 
   render() {
-    return <div>HI</div>;
+    const info = this.props.info;
+
+    if (Object.keys(info).length === 0) {
+      return <div></div>;
+    } else {
+      const activeInfo = info[this.state.active];
+      return (
+        <div class="flex-parent">
+          <div class="button-container">
+            <button
+              id="Safer at Home Advisory"
+              onClick={(e) => this.updateInfo(e.target.id)}
+            >
+              Safer at Home
+            </button>
+          </div>
+          <h3>{this.state.active}</h3>
+          <div class="list-container">
+            {activeInfo.map((item, i) => (
+              <ListItem statement={item} />
+            ))}
+          </div>
+        </div>
+      );
+    }
   }
 }
 
